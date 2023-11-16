2252d6d5a377d356e4d38c82b57707839c124368
RosieDuffy <rose.j.duffy@gmail.com>
2023-11-16 14:15:07 +0000

    Users.js renamed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var booksRouter = require("./routes/books");
 
 const { sequelize } = require("./models");
 
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/books", booksRouter);
 
 // testing connection
 sequelize
